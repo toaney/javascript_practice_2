7aa923b4dcec17157cad3f6b036e40a065e32d41
toaney <toaney@gmail.com>
2021-06-20 10:07:52 -0700

    fix duplicate-letters.js

--- duplicate-letters.js
+++ duplicate-letters.js
@@ -5,27 +5,30 @@
  var lengthOfLongestSubstring = function(s) {
   let longestSubstring = 0;
   let currentSubstring = 0;
-  // let previousLetters = {}; 
   let previousLetters = []
   let currentLetter;
   
   
   for (let i = 0; i < s.length; i++) {
       currentLetter = s[i]
+      
+      // NO PREVIOUS LETTERS
       if (previousLetters.length < 1) {
           previousLetters.push(currentLetter)
           currentSubstring = 1
           if (currentSubstring > longestSubstring) {
               longestSubstring = currentSubstring;
           }
+      // PREVIOUS LETTERS DETECTED
       } else {
           let duplicateCharacter = previousLetters.findIndex(x => x === currentLetter)
+          // DUPLICATE LOGIC
           if (duplicateCharacter !== -1) {
-              // splice 
-              let newSubstring = previousLetters.slice(duplicateCharacter, previousLetters.length + 1)
-              console.log(newSubstring)
+              previousLetters.push(currentLetter)
+              let newSubstring = previousLetters.slice(duplicateCharacter + 1)
               currentSubstring = newSubstring.length
               previousLetters = newSubstring
+          // NON-DUPLICATE LOGIC
           } else {
               previousLetters.push(currentLetter)
               currentSubstring += 1
