b3770812ec6d439bca2b12a2ab9c6716c7d0804f
toaney <toaney@gmail.com>
2021-06-22 16:07:40 -0700

    fix atm-change.js

---
+++ atm-change.js
@@ -0,0 +1,41 @@
+const atmChangeCalculator = (itemPrice, paid, denominationArray) => {
+  let changeAmt = paid - itemPrice;
+  let sortedDenomArray = denominationArray.sort( (a,b) => { return b - a } );
+  let output = {};
+
+  if ( changeAmt < 0 ){
+    console.log(("please insert more money"))
+    return("please insert more money")
+  }
+
+  const calculateDenominations = (change, denominationIndex) => {
+    denom = sortedDenomArray[denominationIndex]
+    while (change >= denom) {
+      change = change - denom;
+      
+      if (!output[denom]){
+        output[denom] = 1;
+      } else {
+        output[denom] += 1;
+      }
+    }
+
+    if (change > 0) {
+      let nextDenominationIndex = denominationIndex + 1
+      if ( nextDenominationIndex > sortedDenomArray.length){
+        console.log("correct change cannot be given")
+        return
+      }
+      calculateDenominations(change, nextDenominationIndex)
+    } 
+  }
+
+  calculateDenominations(changeAmt, 0);
+  console.log(output);
+
+}
+
+// example item price 86, paid 100, denomArray [ 10, 5, 1]
+
+atmChangeCalculator( 86, 100, [ 10, 5, 1])
+
