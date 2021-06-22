e347e7e181083be43191dd4a37f83426d74b1d62
toaney <toaney@gmail.com>
2021-06-22 14:14:06 -0700

    add atm-change.js

--- array-methods.js
+++ array-methods.js
@@ -3,13 +3,46 @@ let arr4 = [ "a", "b", "c", "d", "e", "f" ];
 let arr2 = [ "g", "h", "i", "j", "k", "l" ];
 let arr3 = [ 1, 2, 3, 4, 5 ];
 
+
+
+
+// console.log(arr3.reduce((total, val) => {
+//   return total + val;
+// }, 5))
+
+// let output = arr3.sort((a,b) => {
+//   return b - a;
+// })
+
+// let output = arr3.map()
+
+
+// console.log(output);
+
+
+
+
+// const equals = (arr1, arr2) => {
+//   // return arr1.length === arr2.length && arr1.every( (v, i) => {
+//   //   arr2[i] === v
+//   // })
+
+//   return arr1.length === arr2.length && arr1.every((v,i) => {
+//     return v === arr2[i]
+//   })
+// }
+
+// console.log(equals(arr1, arr4))
+
+
+
 // console.log(arr1 === arr1);
 
-const equals = (a, b) => {
-  return a.length === b.length && a.every((v,i) => v === b[i] )
-}
+// const equals = (a, b) => {
+//   return a.length === b.length && a.every((v,i) => v === b[i] )
+// }
 
-console.log(equals( arr1, arr4 ));
+// console.log(equals( arr1, arr4 ));
 
 
 // console.log(arr3.every( (item) => {
