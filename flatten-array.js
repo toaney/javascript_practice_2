0ace3d948b420785d48348031ea403882ad8842b
toaney <toaney@gmail.com>
2021-06-24 15:33:49 -0700

    more flatten array practice

--- flatten-array.js
+++ flatten-array.js
@@ -5,12 +5,48 @@ let testArray2 = [ 5, 6, 7, [8, 9]];
 let testArray3 = [ 1, 2]
 
 
-const flatten = (acc, item) => {
-  if (Array.isArray(item)){
-    return item.reduce(flatten, acc)
+let flatten = (acc, arr) => {
+  if (Array.isArray(arr)) {
+    return arr.reduce(flatten, acc)
   }
-  acc.push(item)
+  acc.push(arr);
   return acc
 }
 
-console.log(flatten([], testArray));
+console.log(flatten([], testArray))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const flatten = (acc, item) => {
+//   if (Array.isArray(item)){
+//     return item.reduce(flatten, acc)
+//   }
+//   acc.push(item)
+//   return acc
+// }
+
+// console.log(flatten([], testArray));
