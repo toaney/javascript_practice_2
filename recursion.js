6359095cb38371f5f40b0667c258cc0b0571c2a3
toaney <toaney@gmail.com>
2021-06-25 13:57:08 -0700

    add factorial to recursion.js

--- recursion.js
+++ recursion.js
@@ -2,7 +2,7 @@
 let output = [];
 const countdown = (output, num) => {
   if (num >= 0) {
-    console.log(num);
+    // console.log(num);
     output.push(num)
     return countdown(output, num - 1);
   }
@@ -10,3 +10,20 @@ const countdown = (output, num) => {
 }
 
 console.log(countdown(output, 5));
+
+
+// find factorial
+const findFactorial = (num, acc = 1) => {
+  if (num < 0) {
+    console.log("Connot find factorial of a negative number.")
+    return
+  }
+  console.log(acc)
+  if (num > 0) {
+    acc = acc * num
+    return findFactorial(num - 1, acc)
+  }
+  return acc
+}
+
+console.log(findFactorial(0))
