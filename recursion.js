6a491adf4c02866a265715536590b2b59b1bea7f
toaney <toaney@gmail.com>
2021-06-26 21:55:26 -0700

    more factorial practice

--- recursion.js
+++ recursion.js
@@ -12,18 +12,44 @@ const countdown = (output, num) => {
 console.log(countdown(output, 5));
 
 
-// find factorial
-const findFactorial = (num, acc = 1) => {
+
+const findFactorial = (num) => {
   if (num < 0) {
-    console.log("Connot find factorial of a negative number.")
-    return
+    
   }
-  console.log(acc)
-  if (num > 0) {
-    acc = acc * num
-    return findFactorial(num - 1, acc)
-  }
-  return acc
 }
 
-console.log(findFactorial(0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// find factorial
+// const findFactorial = (num, acc = 1) => {
+//   if (num < 0) {
+//     console.log("Connot find factorial of a negative number.")
+//     return
+//   }
+//   console.log(acc)
+//   if (num > 0) {
+//     acc = acc * num
+//     return findFactorial(num - 1, acc)
+//   }
+//   return acc
+// }
+
+// console.log(findFactorial(0))
